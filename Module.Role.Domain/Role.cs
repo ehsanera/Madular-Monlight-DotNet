48cfedec49d6c52a8f59e60f0ffc83646822d6f5
alireza995 <ma.sayedbagheri@gmail.com>
2022-07-09 16:31:27 +0430

    Add role module with crud

---
+++ Module.Role.Domain/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Module.Role.Domain;
+
+public class Role
+{
+    public int Id { get; set; }
+
+    [MaxLength(255)]
+    public string Name { get; set; }
+
+    [MaxLength(4000)]
+    public string? Description { get; set; }
+}
